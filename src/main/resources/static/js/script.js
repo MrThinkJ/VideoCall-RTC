d3b6459a2d6ce1235fb71f3888b4fe4e183ec720
MrThinkJ <ledinhthinh.ws@gmail.com>
2024-06-20 10:11:39 +0700

    Feature: Stream video from camera, adding SSL

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,121 +1,173 @@
-// Initial connection to websocket for signaling server
-const conn = new WebSocket("ws://localhost:8080/socket");
-conn.onopen = ()=>{
-    console.log("Connected to the signaling server");
-    initialize();
-}
-conn.onmessage = (message)=>{
-    console.log("New message: "+message.data);
-    const signal = JSON.parse(message.data)
-    const type = signal.type;
-    switch (type){
-        case "init":
-            createOffer(signal);
-            break;
-        case "offer":
-            handleOffer(signal);
-            break;
-        case "answer":
-            handleAnswer(signal);
-            break;
-        case "ice":
-            handleIce(signal);
-            break;
-        case "logout":
-            handleLogout(signal);
-            break;
-    }
-}
-function send(message){
-    conn.send(JSON.stringify(message));
-}
 let peerConnection;
 let dataChannel;
 let otherClient;
+let websocketConn;
+const PORT = "8443";
+const SOCKET = "/socket";
 const input = document.getElementById("messageInput");
-function initialize(){
-// Create peerConnection, dataChannel and also the event of dataChannel
-    const configuration = null;
-    peerConnection = new RTCPeerConnection(configuration);
-    peerConnection.onicecandidate = (event)=>{
-        if (event.candidate){
-            send({
-                type: "ice",
-                receiver: otherClient,
-                data: event.candidate
-            })
-        }
-    };
-    dataChannel = peerConnection.createDataChannel("dataChannel");
-    dataChannel.onopen = (event)=>{
-        console.log("Data channel is opened");
-    }
-    dataChannel.onmessage = (event)=>{
-        console.log("New message: "+event.data);
-    }
-    dataChannel.onclose = (event)=>{
-        console.log("Data channel is closed");
-    }
-    dataChannel.onerror = (error) =>{
-        console.log("Data channel has error: "+error);
-    }
-    peerConnection.ondatachannel = (event) =>{
-        dataChannel = event.channel;
-        dataChannel.onmessage = (event)=>{
-            console.log("On datachannel message: "+event.data);
-        }
-    };
-}
-
-function createOffer(signal){
-    const senderId = signal.sender;
-    otherClient = senderId;
-    peerConnection.createOffer().then((sdp)=>{
-        peerConnection.setLocalDescription(sdp).then();
-        console.log("Create offer for: "+senderId);
-        send({
-            type: "offer",
-            receiver: senderId,
-            data: sdp
-        });
-    }).catch((error)=>{
-        console.log("Error: "+error);
+const constraints = {
+  video: true,
+  audio: true,
+};
+const configuration = {
+  iceServers: [{ urls: "stun:stun2.1.google.com:19302" }],
+};
+let localStream;
+const selfView = document.getElementById("selfView");
+const remoteView = document.getElementById("remoteView");
+function initialize() {
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((stream) => {
+      localStream = stream;
+      selfView.srcObject = new MediaStream([localStream.getTracks()[1]]);
+      setUpPeerConnection();
+      setUpWebSocket();
     })
-}
-
-function handleOffer(signal){
-    otherClient = signal.sender;
-    peerConnection.setRemoteDescription(new RTCSessionDescription(signal.data)).then(r=>{
-        peerConnection.createAnswer().then((sdp)=>{
-            peerConnection.setLocalDescription(sdp).then();
-            send({
-                type: "answer",
-                receiver: signal.sender,
-                sender: signal.receiver,
-                data: sdp
-            })
-        }).catch((err)=>console.log("Error while create answer: "+err));
+    .catch((err) => {
+      console.log("Error when stream: " + err);
+      alert(err);
     });
 }
 
-function handleIce(signal){
-    if (signal.data){
-        console.log("Adding ICE");
-        peerConnection.addIceCandidate(new RTCIceCandidate(signal.data)).catch((error)=>console.error("Error: "+error));
+function setUpPeerConnection() {
+  peerConnection = new RTCPeerConnection(configuration);
+  peerConnection.onicecandidate = (event) => {
+    if (event.candidate) {
+      send({
+        type: "ice",
+        receiver: otherClient,
+        data: event.candidate,
+      });
     }
-}
+  };
+  dataChannel = peerConnection.createDataChannel("dataChannel");
+  dataChannel.onopen = (event) => {
+    console.log("Data channel is opened");
+  };
+  dataChannel.onmessage = (event) => {
+    console.log("New message: " + event.data);
+  };
+  dataChannel.onclose = (event) => {
+    console.log("Data channel is closed");
+  };
+  dataChannel.onerror = (error) => {
+    console.log("Data channel has error: " + error);
+  };
 
-function handleAnswer(signal){
-    peerConnection.setRemoteDescription(new RTCSessionDescription(signal.data)).then(()=>{
-        console.log('Setting remote description by answer from: ' + signal.sender);
+  peerConnection.ondatachannel = (event) => {
+    dataChannel = event.channel;
+    dataChannel.onmessage = (event) => {
+      console.log("On datachannel message: " + event.data);
+    };
+  };
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  peerConnection.addEventListener("track", async (event) => {
+    console.log(event);
+    const [remoteStream] = event.streams;
+    remoteView.srcObject = remoteStream;
+  });
+}
+function setUpWebSocket() {
+  websocketConn = new WebSocket("wss://"+window.location.hostname+":"+PORT+SOCKET);
+  websocketConn.onopen = () => {
+    console.log("Connected to the signaling server");
+  };
+  websocketConn.onmessage = (message) => {
+    console.log("New message: " + message.data);
+    const signal = JSON.parse(message.data);
+    const type = signal.type;
+    switch (type) {
+      case "init":
+        createOffer(signal);
+        break;
+      case "offer":
+        handleOffer(signal);
+        break;
+      case "answer":
+        handleAnswer(signal);
+        break;
+      case "ice":
+        handleIce(signal);
+        break;
+      case "logout":
+        handleLogout(signal);
+        break;
+    }
+  };
+}
+function send(message) {
+  websocketConn.send(JSON.stringify(message));
+}
+function createOffer(signal) {
+  const senderId = signal.sender;
+  otherClient = senderId;
+  peerConnection
+    .createOffer()
+    .then((sdp) => {
+      peerConnection.setLocalDescription(sdp).then();
+      console.log("Create offer for: " + senderId);
+      send({
+        type: "offer",
+        receiver: senderId,
+        data: sdp,
+      });
+    })
+    .catch((error) => {
+      console.log("Error: " + error);
     });
 }
 
-function handleLogout(signal){
-    console.log(signal.sender+ " logout!");
+function handleOffer(signal) {
+  otherClient = signal.sender;
+  peerConnection
+    .setRemoteDescription(new RTCSessionDescription(signal.data))
+    .then((r) => {
+      peerConnection
+        .createAnswer()
+        .then((sdp) => {
+          peerConnection.setLocalDescription(sdp).then();
+          send({
+            type: "answer",
+            receiver: signal.sender,
+            sender: signal.receiver,
+            data: sdp,
+          });
+        })
+        .catch((err) => console.log("Error while create answer: " + err));
+    });
+}
+
+function handleIce(signal) {
+  if (signal.data) {
+    console.log("Adding ICE");
+    peerConnection
+      .addIceCandidate(new RTCIceCandidate(signal.data))
+      .catch((error) => console.error("Error: " + error));
+  }
+}
+
+function handleAnswer(signal) {
+  peerConnection
+    .setRemoteDescription(new RTCSessionDescription(signal.data))
+    .then(() => {
+      console.log(
+        "Setting remote description by answer from: " + signal.sender
+      );
+    });
+}
+
+function handleLogout(signal) {
+  console.log(signal.sender + " logout!");
+  dataChannel.close();
 }
 
 function sendMessage() {
-    dataChannel.send(input.value);
-    input.value = "";
+  dataChannel.send(input.value);
+  input.value = "";
 }
+
+window.onload = initialize;
